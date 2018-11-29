40d365a9e700fa98254c7aff19cf2a2f1b5d90b6
Max Garin <maxgarin@gmail.com>
2018-11-29 13:35:09 -0500

    Added user registration and login

--- _config.py
+++ _config.py
@@ -4,8 +4,6 @@ import os
 # grab the folder where this script lives
 basedir = os.path.abspath(os.path.dirname(__file__))
 DATABASE = 'flasktaskr.db'
-USERNAME = 'admin'
-PASSWORD = 'admin'
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'myprecious'
 
